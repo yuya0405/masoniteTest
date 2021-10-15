ae975e8f8139850cb64946b7112d979cd4d8dae3
Yuya Ohbayashi <yuya9445@gmail.com>
2021-10-15 21:02:06 +0900

    craft Controller Some

---
+++ project/app/http/controllers/SomeController.py
@@ -0,0 +1,20 @@
+"""A SomeController Module."""
+
+from masonite.request import Request
+from masonite.view import View
+from masonite.controllers import Controller
+
+
+class SomeController(Controller):
+    """SomeController Controller Class."""
+
+    def __init__(self, request: Request):
+        """SomeController Initializer
+
+        Arguments:
+            request {masonite.request.Request} -- The Masonite Request class.
+        """
+        self.request = request
+
+    def show(self, view: View):
+        pass
